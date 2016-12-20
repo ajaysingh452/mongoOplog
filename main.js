39f990b83db419132b8526d46fe8f4755d7635a8
ajaysingh452 <ajaysingh452@live.com>
2016-12-20 21:14:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,33 @@
-
 //Includes
 var http=require("http");
-var postal=require("postal");
 var mongoose = require('mongoose');
-
 //Global Varibles
 var db = mongoose.connect('mongodb://localhost/local');  //local db has oplogs collection
 var mongoData=[];
-var channel=postal.channel('alertData');
-
 // Read oplog from MongoDB
 mongoose.connection.on('open', function callback() {
-    var collection = mongoose.connection.db.collection('oplog.rs'); //or any capped collection
-    
+    var collection = mongoose.connection.db.collection('oplog.rs'); //or any capped collection    
 	var stream = collection.find({}, {
         tailable: true,
         awaitdata: true,
         numberOfRetries: Number.MAX_VALUE
-    }).stream(); 	
-	
+    }).stream(); 		
 	stream.on('data', function(val) {		
-		if(val.ns=='Hero_Dev.UC_MST_User')
+		if(val.ns=='test.testCollection')
 		{		
-			mongoData.push(val);
-		 // channel.publish("alert.add",{data:val});
+			mongoData.push(val);		 
 		}
 	});	
 	
 	stream.on('error', function(val) {
-		console.log('Error: %j', val);
-		
+		console.log('Error: %j', val);		
 	});	
 
 	stream.on('end', function(){
 		console.log('End of stream');		
 	});	
-	
-
 });	
-
-
 var server=http.createServer(function(request,response){
-	
-	/*channel.subscribe("alert.add",function(res){
-		console.log(res.ns);
-	});	*/
-	
 		if(mongoData.length!=0)
 		{
 			var completeData='';
@@ -54,8 +36,6 @@ var server=http.createServer(function(request,response){
 				var data={"collection":mongoData[i].ns,
 						  "data":mongoData[i].o,
 						  "operation":mongoData[i].op}
-				//completeData+= mongoData[i].ns+"\n"+JSON.stringify(mongoData[i].o)+"\n"+mongoData[i].op+"\n\n\n";
-				
 				completeData+= JSON.stringify(data)+"\n\n\n";
 			}
 			response.end(completeData);
@@ -66,6 +46,5 @@ var server=http.createServer(function(request,response){
 		}		
 		mongoData=[];
 });
-
 server.listen(9090);
 console.log("Server Is Running on port 9090");
